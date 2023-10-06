beb283babebdfa4f47e341d5c234e86a02202a26
pavanmanikrao-hyniva <pavan.manikrao@hyniva.com>
2023-10-06 10:47:12 +0530

    statusCode-com

--- app.js
+++ app.js
@@ -3,88 +3,96 @@ const {
     GetItemCommand, // Retrieve data fron dynamoDb table
     DynamoDBClient, // Dynamodb instance
     ScanCommand, //Scan the table
-  } = require('@aws-sdk/client-dynamodb'); //aws-sdk is used to build rest APIs,
+} = require('@aws-sdk/client-dynamodb'); //aws-sdk is used to build rest APIs,
 //client-dynamodb library used to communicate with the
-  //create new instance of DynamoDBClient to db, will use this constant across the program
-const db = new DynamoDBClient(); 
-//import util-dynamodb
-const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb'); // retrieve and save data
+//Create new instance of DynamoDBClient to db, will use this constant across the program
+const db = new DynamoDBClient();
+const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb'); // Retrieve and save data
 
-//this function will get employee details based on empId 
-//create function as async with event as argument
+//This function will get employee details based on empId 
+//Create function as async with event as argument
 const getEmployee = async (event) => {
-    //initialize status code 200 OK 
+    //Initialize status code 200 OK 
     const response = { statusCode: 200 };
-    //try block code
-    try { 
-        // define tablename and employeeId key with its value
+    //Try block code - this block evaluates the employee retrieve function based on empId,
+    // If true it gives employee details or it catches server response error and displayes at console
+    try {
+        // Define tablename and employeeId key with its value
         const params = {
             TableName: process.env.DYNAMODB_TABLE_NAME,
             Key: marshall({ empId: event.pathParameters.empId }),
         };
-        //await response from db when sent getItem command with params 
-        //containing tablename, key and only display empId and personalInfo
+        //Await response from db when sent GetItemCommand 
+        //With params as argument containing tablename and key
         const { Item } = await db.send(new GetItemCommand(params));
 
         if (Item) {
             response.body = JSON.stringify({
-              message: "Successfully retrieved employee.",
-              data: unmarshall(Item)
+                message: "Successfully retrieved employee details.",
+                data: unmarshall(Item)
             });
-          } else if(Item == {}){
-            // throw an error if the item is not found
-            throw new Error('Employee details not found.');
-          }
-          else {
+        } else if (Item === undefined) {
+            response.statusCode = 404;
+            response.body = JSON.stringify({
+                message: 'Employee details not found.'
+            });
+        }
+        else {
             throw new Error('Unexpected error occurred.');
-          }
-    } // catch block to handle any errors
+        }
+    } // Catch block to handle any errors
     catch (e) {
         console.error(e);
-        response.statusCode(e.message === 'Employee details not found.' ? 400 : e.statusCode || 500);
         response.body = JSON.stringify({
-            statusCode : response.statusCode,
-            message: "Failed to get employee.",
+            statusCode: response.statusCode,
+            message: "Failed to get employee details.",
             errorMsg: e.message,
             errorStack: e.stack,
         });
     }
-    //return response with statusCode and data.
+    //Return response with statusCode and data.
     return response;
 };
 
 //This function is used to retrieve all employees details
-//create getAllEmployees function as async
-const getAllEmployees = async () => {   
+//Create getAllEmployees function as async
+const getAllEmployees = async () => {
     const response = {};
-    //try block code - this block evaluates the employee retrieve function, if true it gives employee details
+    //Try block code - this block evaluates the employee retrieve function, if true it gives employee details
     //or if false, it catches server response error and displayes at console
     try {
         const input = {
             TableName: process.env.DYNAMODB_TABLE_NAME,
-          };
-        //await response from db when sent scan command with tablename
+        };
+        //Await response from db when sent scan command with tablename
         const { Items } = await db.send(new ScanCommand(input));
-        // generate response message and body
-        response.body = JSON.stringify({
-            message: "Successfully retrieved all employees.",
-            data: Items?.map((item) => unmarshall(item)),
-        });
-    } 
-    // catch block to handle any server response errors
+        if (Items.length === 0) { // If employees are not present
+            response.statusCode = 404; // Setting the status code to 404
+            response.body = JSON.stringify({
+                message: "Employees details not found.",
+            });
+        } else {
+            // Generate response message and data
+            response.body = JSON.stringify({
+                message: "Successfully retrieved all employees.",
+                data: Items?.map((item) => unmarshall(item)),
+            });
+        }
+    }
+    // Catch block to handle any server response errors
     catch (e) {
         console.error(e);
-            response.body = JSON.stringify({
+        response.body = JSON.stringify({
             message: "Failed to retrieve all employees.",
             errorMsg: e.message,
             errorStack: e.stack,
         });
     }
-    //return response with statusCode and data. 
+    //Return response with statusCode and data. 
     return response;
 };
 
-//exporting methods globally
+//Exporting methods globally
 module.exports = {
     getEmployee,
     getAllEmployees,
