abeed1f1ac341ee86f1bb271141ba1dd0e3870f2
pavanmanikrao-hyniva <pavan.manikrao@hyniva.com>
2023-10-11 11:18:58 +0530

    final-testcases

--- test.js
+++ test.js
@@ -16,46 +16,62 @@ describe('get employee by empId unit tests', () => {
     });
 
     //valid empId test case
-    it(`Should return employee bank details for valid empId`, async () => {
+    it(`Should return employee details for valid empId`, async () => {
         // Mock event object
         DynamoDBClient.prototype.send = async function () {
             // Create a mock send function that returns mock data
             const mockItem = { empId: "1233" };
             return { Item: marshall(mockItem) };
         };
-        let event = {
+        const event = {
+            headers: {
+                Accept: '*/*',
+            },
+            body: {},
+            resource: '/employee/{empId}',
+            path: '/employee/1001',
+            httpMethod: 'GET',
             pathParameters: {
                 empId: '1001',
             },
         };
+        const empId = event.pathParameters.empId;
         // calling the getEmployee from the app.js file
         const response = await getEmployee(event);
         expect(response.statusCode).to.be.equals(200);
         const responseBody = JSON.parse(response.body);
         expect(responseBody.message).to.equal(
-            'Successfully retrieved employee details.'
-        );
-    });
-
-    //invalid empId test case
-    it('Employee details not found error for invalid empId', async () => {
-        // Create a mock send function that returns an empty response
-        DynamoDBClient.prototype.send = async function () {
-            return {};
-        };
-        //invalid empId
-        let event = {
-            pathParameters: {
-                empId: '10',
-            },
-        };
-        // calling the getEmployee from the app.js file
-        const response = await getEmployee(event);
+            `Successfully retrieved employee details of empId : ${empId}.`
+            );
+        });
+        
+        //invalid empId test case
+        it('Employee details not found error for invalid empId', async () => {
+            // Create a mock send function that returns an empty response
+            DynamoDBClient.prototype.send = async function () {
+                return {};
+            };
+            //invalid empId
+            const event = {
+                headers: {
+                    Accept: '*/*',
+                },
+                body: {},
+                resource: '/employee/{empId}',
+                path: '/employee/10',
+                httpMethod: 'GET',
+                pathParameters: {
+                    empId: '10',
+                },
+            };
+            const empId = event.pathParameters.empId;
+            // calling the getEmployee from the app.js file
+            const response = await getEmployee(event);
         expect(response.statusCode).to.equal(404);
         const responseBody = JSON.parse(response.body);
         expect(responseBody.message).to.equal(
-            'Employee details not found.'
-        );
+            `Employee details not found for empId : ${empId}.`
+            );
     });
 
     //unexpected error test case for get by empID
@@ -65,16 +81,24 @@ describe('get employee by empId unit tests', () => {
             throw new Error('Unexpected error occurred.');
         };
         //invalid empId
-        let event = {
+        const event = {
+            headers: {
+                Accept: '*/*',
+            },
+            body: {},
+            resource: '/employee/{empId}',
+            path: '/employee/10',
+            httpMethod: 'GET',
             pathParameters: {
                 empId: '10',
             },
         };
+        const empId = event.pathParameters.empId;
         // calling the getEmployee from the app.js file
         const response = await getEmployee(event);
         const responseBody = JSON.parse(response.body);
         expect(responseBody.message).to.equal(
-            'Failed to get employee details.'
+            `Failed to get employee details with empId : ${empId}.`
         );
     });
     //successfully get all employeees
@@ -101,7 +125,20 @@ describe('get employee by empId unit tests', () => {
                 // Simulate a successful scan operation with mock data
                 return { Items: mockItems.map((item) => marshall(item)) };
             };
-            const response = await getAllEmployees();
+
+            const event = {
+                headers: {
+                    Accept: '*/*',
+                },
+                body: {},
+                resource: '/employees',
+                path: '/employees',
+                httpMethod: 'GET',
+                pathParameters: {
+                    empId: '10',
+                },
+            };
+            const response = await getEmployee(event);
             expect(response.statusCode).to.be.equals(200);
             const responseBody = JSON.parse(response.body);
             expect(responseBody.message).to.equal(
@@ -116,21 +153,69 @@ describe('get employee by empId unit tests', () => {
                 // Simulate a successful scan operation with mock data
                 return { Items: [] };
             };
-            const response = await getAllEmployees();
+            const event = {
+                headers: {
+                    Accept: '*/*',
+                },
+                body: {},
+                resource: '/employees',
+                path: '/employees',
+                httpMethod: 'GET',
+                pathParameters: {
+                    empId: '10',
+                },
+            };
+            const response = await getEmployee(event);
             expect(response.statusCode).to.equal(404);
             const responseBody = JSON.parse(response.body);
             expect(responseBody.message).to.equal('Employees details are not found.');
         });
         //Unexpected error while get all employees test case
-        it('Unexpected error while - get all employees', async () => {
+        it('Unexpected error while - get all employees API call', async () => {
             // Mock an error by changing the DynamoDBClient behavior
             DynamoDBClient.prototype.send = async () => {
                 throw new Error('intentional error : Failed to get all employees.');
             };
-            const response = await getAllEmployees();
+            const event = {
+                headers: {
+                    Accept: '*/*',
+                },
+                body: {},
+                resource: '/employees',
+                path: '/employees',
+                httpMethod: 'GET',
+                pathParameters: {
+                    empId: '10',
+                },
+            };
+            const response = await getEmployee(event);
             const responseBody = JSON.parse(response.body);
             expect(responseBody.message).to.equal('Failed to retrieve all employees.');
             expect(responseBody.errorMsg).to.equal('intentional error : Failed to get all employees.');
+        });
+    });
+
+    //wrong endpoint test
+    describe('wrong endpoint test', () => {
+        it('Should get 404 error If wrogn endpoint is present', async () => {
+                const event = {
+                headers: {
+                    Accept: '*/*',
+                },
+                body: {},
+                resource: '/employees/g',
+                path: '/employees/g',
+                httpMethod: 'GET',
+                pathParameters: {
+                    empId: '10',
+                },
+            };
+            const response = await getEmployee(event);
+            expect(response.statusCode).to.be.equals(404);
+            const responseBody = JSON.parse(response.body);
+            expect(responseBody.message).to.equal(
+                `URL not found -  ${event.resource}`
+            );
         });
     });
 
